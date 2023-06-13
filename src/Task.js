1fd9555fe199732fb26ab84fb2d623c3e073e612
hkennandya9 <hkennandya@gmail.com>
2023-06-14 02:26:52 +0700

    update task.js

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const url = "https://monumental-starburst-f363fa.netlify.app/" // TODO: replace this
+const studentName = "Hafizh Kennandya Maulana" // TODO: replace this
+const studentId = "FE4593655" // TODO: replace this
 
 export { url, studentName, studentId }
