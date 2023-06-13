d908a42d86f2765fdbec3ff0ef4688caacf82c7d
hkennandya9 <hkennandya@gmail.com>
2023-06-14 02:20:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ const App = () => {
     <>
       <div className='navbar'>
         <div className='studentInfo'>
-          {/* TODO: answer here */}
+          <h2 className="studentName">Hafizh Kennandya Maulana</h2>
+          <p className="studentId">FE4593655</p>
         </div>
         <Link to='/'>Home</Link>
         <Link to='/photos'>My Photos</Link>
